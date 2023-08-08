b5a8a6536593f4b613a6b2f9ddfad44f82d38b68
100rabhsharpcoder <saurabhgathade@gmail.com>
2023-08-08 18:20:37 +0530

    Finishing up

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,109 +1,54 @@
 import React from 'react';
 
-class CartItem extends React.Component{
- 
-
-    // testing(){
-    //     const promise =  new Promise((resolve, reject )=>{
-    //         setTimeout(()=>{
-    //             resolve('done');
-    //         },5000);
-    //     })
-    //     promise.then(()=>{
-    //         this.setState({qty:this.state.qty+10});
-            
-    //         // set state acts like asycnhronou call
-    //         console.log("state", this.state);
-    //     })
-    // }
+const CartItem = (props) => {
 
 
-    // function for increase quantity 
-    increaseQuantity = () =>{
-        // this.state.qty+=1;
-        // set sate form one
-        // this.setState({
-        //     qty:this.state.qty+1
-        // });
+    console.log("this.prop", props)
+    const { price, title, qty } = props.product;
+    const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = props;
+    return (
+        <div className='card-item' style={{ display: 'inline-flex' }}>
+            {/* {this.props.jsx} */}
+            <div className='left-block'>
+                <img style={styles.image} />
+            </div>
 
-        // set state form 2 - if previousstate requred use this form 
-        this.setState((prevState)=>{
-                 return{
-                   qty: prevState.qty+1
-                 }
-        }, ()=>{ 
-            console.log('this.state', this.state);
-        });
-        
-        
-    }
-   
+            <div className='right-block'>
+                <div style={{ fontSize: 25 }}>{title}</div>
+                <div style={{ color: '#777' }}>Rs {price}</div>
+                <div style={{ color: '#777' }}>Qty:{qty}</div>
+                <div className='cart-item-actions'></div>
 
-    // function for decrase the quantity
-    decreaseQuantity = () =>{
-        const {qty} =  this.state;
-        if(qty === 0){
-            return;
-        }
-        // this.state.qty+=1;
-        // set sate form one
-        // this.setState({
-        //     qty:this.state.qty-1
-        // });
+                {/* Buttons */}
+                <img alt="increase"
+                    className="action-icons"
+                    src="https://www.svgrepo.com/show/522735/add-circle.svg"
+                    onClick={() => onIncreaseQuantity(product)} />
 
-        // set state form 2 - if previousstate requred use this form 
-        this.setState((prevState)=>{
-                 return{
-                   qty: prevState.qty-1
-                 }
-        });
-        // console.log('this.state', this.state);
-    }
-    render(){
-        // console.log("this.prop", this.props)
-        const { price , title , qty} = this.props.product;
-        return (
-             <div className='card-item' style={{display: 'inline-flex'}}>
-                {this.props.jsx}
-                <div className='left-block'>
-                     <img style={styles.image}/>
-                </div>
+                <img alt="decrease"
+                    className="action-icons"
+                    src="https://www.svgrepo.com/show/522961/minus-circle.svg "
+                    onClick={() => onDecreaseQuantity(product)} />
 
-                <div className='right-block'>
-                     <div style={{fontSize:25}}>{title}</div>
-                     <div style={{color:'#777'}}>Rs {price}</div>
-                     <div style={{color:'#777'}}>Qty:{qty}</div>
-                     <div className='cart-item-actions'></div>
+                <img alt="delete"
+                    className="action-icons"
+                    src="https://www.svgrepo.com/show/502608/delete-2.svg"
+                    onClick={() => onDeleteProduct(product.id)} />
 
-                      {/* Buttons */}
-            <img alt="increase"
-             className="action-icons" 
-             src="https://www.svgrepo.com/show/522735/add-circle.svg"
-             onClick={this.increaseQuantity} />
-
-            <img alt="decrease" 
-            className="action-icons"
-             src="https://www.svgrepo.com/show/522961/minus-circle.svg "
-             onClick={this.decreaseQuantity} />
-
-            <img alt="delete"
-             className="action-icons" 
-             src= "https://www.svgrepo.com/show/502608/delete-2.svg" />
-
-                </div>
+            </div>
 
 
-             </div>
-        );
-    }
+        </div>
+    );
 }
 
+
 const styles = {
-    image:{
+    image: {
         height: 110,
         width: 110,
-        borderRadius:4,
-        background:'#ccc'
+        borderRadius: 4,
+        background: '#ccc'
     }
 }
 
